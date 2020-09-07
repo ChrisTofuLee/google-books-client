41707c9817e32c60503fe59fc8a69060fbf37d21
Selfay <chris.lee90@hotmail.co.uk>
2020-09-07 21:55:22 +0100

    Updates

---
+++ precache-manifest.b8f40e4ddf316f299f7cfad3af36c98f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60890cf71daeb89e56a21eaf3ecf76e",
+    "url": "/google-books-client/index.html"
+  },
+  {
+    "revision": "7fddd4d87d2b9ef114bc",
+    "url": "/google-books-client/static/js/2.cb3f86db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/google-books-client/static/js/2.cb3f86db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c90ab32a158428b09ab",
+    "url": "/google-books-client/static/js/main.f3c728ba.chunk.js"
+  },
+  {
+    "revision": "2f4606b7ccc9863de05f",
+    "url": "/google-books-client/static/js/runtime-main.7a2912b8.js"
+  }
+]);
